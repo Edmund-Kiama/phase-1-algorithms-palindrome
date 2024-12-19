dc3b8051be9d2c3b0e515a156db8b5a6c7f0882b
Edmund <edmundgachanja@gmail.com>
2024-12-19 08:53:56 +0300

    finishes lab

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 function isPalindrome(word) {
-  // iterate from the beginning of the string to the middle of the string
   for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
-    // compare the letter we're iterating over to the corresponding letter at the end of the string
     const endIndex = word.length - 1 - startIndex;
     if (word[startIndex] !== word[endIndex]) {
-      // if the letters don't match, return false
       return false;
     }
   }
 
-  // if we reach the middle, and all the letters match, return true
   return true;
 }
 
@@ -53,6 +49,11 @@ if (require.main === module) {
 
   console.log("Expecting: true");
   console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 }
